a099b0c510522d3393f8d419c7abfd38ccf7293b
VUILLIN Thomas <144356846+vuillin@users.noreply.github.com>
2023-10-17 17:59:56 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
+var imageIndex = 1;
 
+function slideEquipe() {
+
+    imageIndex++;
+
+    if (imageIndex == 4)
+      imageIndex = 1
+
+    equipe1.style.display = 'none';
+    equipe2.style.display = 'none';
+    equipe3.style.display = 'none';
+
+    prenomsEquipe1.style.display = 'none';
+    prenomsEquipe2.style.display = 'none';
+    prenomsEquipe3.style.display = 'none';
+
+   switch (imageIndex) {
+
+    case 1:
+      equipe1.style.display = 'block';
+      prenomsEquipe1.style.display = 'block';
+      break;
+
+    case 2:
+      equipe2.style.display = 'block';
+      prenomsEquipe2.style.display = 'block';
+      break;
+
+    case 3:
+      equipe3.style.display = 'block';
+      prenomsEquipe3.style.display = 'block';
+      break;
+
+   }
+
+}
+
+setInterval(slideEquipe, 7000);
